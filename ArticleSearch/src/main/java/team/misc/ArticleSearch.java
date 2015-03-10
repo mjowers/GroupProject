441c224136b729c956fb2dc8827e691e3f411485
slckwd <s.lckwd@gmail.com>
2015-03-10 15:36:54 -0400

    Initial commit

---
+++ ArticleSearch/src/main/java/team/misc/ArticleSearch.java
@@ -0,0 +1,13 @@
+package team.misc;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArticleSearch 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

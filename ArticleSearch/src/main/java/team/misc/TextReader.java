67233d4414b46508f526e84cc6ba66cf2e858b15
mjowers <melissa.anne.jowers@gmail.com>
2015-03-12 13:22:00 -0400

    Cleaned up branch merge
    got rid of duplicate code from merge of branches

--- ArticleSearch/src/main/java/team/misc/TextReader.java
+++ ArticleSearch/src/main/java/team/misc/TextReader.java
@@ -3,10 +3,8 @@ package team.misc;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-<<<<<<< HEAD
-import java.nio.file.FileSystems;
-=======
->>>>>>> Thursday
+
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -16,13 +14,11 @@ public class TextReader {
 	public static ArrayList<String> readFile (Path filePath) {
 		ArrayList<String> textFromFile = new ArrayList<String> ();
 		try {
-<<<<<<< HEAD
+
 
 			BufferedReader reader = Files.newBufferedReader(filePath,StandardCharsets.UTF_8);
 
-=======
-			BufferedReader reader = Files.newBufferedReader(filePath,StandardCharsets.UTF_8);
->>>>>>> Thursday
+
 			String lines;
 			while ((lines = reader.readLine()) != null) {
 				textFromFile.add(lines);

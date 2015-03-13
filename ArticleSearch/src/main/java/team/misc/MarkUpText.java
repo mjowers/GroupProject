cdb44a08d541fc5ab4bb9e5fc42e4bf82c53bb7f
slckwd <s.lckwd@gmail.com>
2015-03-13 11:17:05 -0400

    Added MarkUp functionality

---
+++ ArticleSearch/src/main/java/team/misc/MarkUpText.java
@@ -0,0 +1,19 @@
+package team.misc;
+
+import java.util.ArrayList;
+
+public class MarkUpText {
+
+	public static String markUp (String text, ArrayList <String> words) {
+		if (text == null || words.size() < 1 || text == "") {
+			return text;
+		}
+		else if (words.size() == 1 && words.get(0) == "") {
+			return text;
+		}
+		for (String word : words) {
+			text = text.replaceAll(word, "<mark>" + word + "</mark>");
+	
+		}		return text;
+	}
+}
---
+++ ArticleSearch/src/test/java/team/misc/MarkUpTextTest.java
@@ -0,0 +1,84 @@
+package team.misc;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MarkUpTextTest {
+	String text;
+	ArrayList <String> mark = new ArrayList<>();
+	String expected;
+	String actual;
+	
+	@Before //before it runs each test, it'll do these things
+	public void initialize() {
+		text = null;
+		mark.clear();
+		expected = null;
+		actual = null;
+	}
+	@Test
+	public void textAndMarkNull() {
+		actual = MarkUpText.markUp(text, mark); 
+		expected = null;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void textNull() {
+		mark.add("mark");
+		actual = MarkUpText.markUp(text, mark); 
+		expected = null;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void markNull() {
+		text = "text";
+		actual = MarkUpText.markUp(text, mark); 
+		expected = "text";
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void markNoString() {
+		mark.add("");
+		text = "text";
+		actual = MarkUpText.markUp(text, mark); 
+		expected = "text";
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void oneWordMarkUp() {
+		text = "text";
+		mark.add("text");
+		actual = MarkUpText.markUp(text, mark); 
+		expected = "<mark>text</mark>";
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void stringMarkUp() {
+		text = "this text is longer now";
+		mark.add("text");
+		actual = MarkUpText.markUp(text, mark); 
+		expected = "this <mark>text</mark> is longer now";
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void multiWordMarkUp() {
+		text = "this text is longer now";
+		mark.add("text");
+		mark.add("longer");
+		actual = MarkUpText.markUp(text, mark); 
+		expected = "this <mark>text</mark> is <mark>longer</mark> now";
+		assertEquals(expected, actual);
+	}
+	
+	
+}

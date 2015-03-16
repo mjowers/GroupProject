7c9b86ce7f49aa9f4144c2a6cae37785ed3ee0df
cammori <cammori@gmail.com>
2015-03-16 14:58:55 -0400

    Added customization to main
    Updated the main for user input

--- ArticleSearch/src/main/java/team/misc/ArticleSearch.java
+++ ArticleSearch/src/main/java/team/misc/ArticleSearch.java
@@ -1,8 +1,12 @@
 package team.misc;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -10,35 +14,92 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class ArticleSearch {
+
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException {
 		String sep = File.separator;
 		String resPath = new File("").getAbsolutePath() + sep + "src" + sep
 				+ "main" + sep + "resources";
+		Path wordsPath = null;
 
-		Path wordsPath = Paths.get(resPath + sep + "words.txt");
+		boolean ok = false;
+		String f = null;
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(System.in));
+		do {
+			System.out
+					.println("Please enter a file name, or return to access the default words.txt: ");
+			f = bufferedReader.readLine();
+			if (f.equals("")) {
+				wordsPath = Paths.get(resPath + sep + Constants.WORDS_TXT);
+				ok = true;
+			} else if (new File(resPath + sep + f).exists()) {
+				wordsPath = Paths.get(resPath + sep + f);
+				ok = true;
+			} else
+				System.err.println("Doesn't exist or is not a file.");
+		} while (!ok);
+
+		ArrayList<String> urlList = new ArrayList<String>();
+		do {
+			System.out
+					.println("Please enter a URL, or press enter to access the default urlslist.txt: ");
+			
+			f = bufferedReader.readLine();
+			if (f.equals("")) {
+				Path urlPath = Paths
+						.get(resPath + sep + Constants.URLSLIST_TXT);
+				FileReaderObject urls = new FileReaderObject(urlPath);
+				urlList = urls.getRawText();
+				ok = true;
+			} else {
+				try {
+					URL url = new URL(f);
+					urlList.add(f);
+					ok = true;
+				} catch (MalformedURLException mue) {
+					ok = false;
+					System.err.println("Bad URL.");
+				}
+			}
+		} while (!ok);
+		bufferedReader.close();
+		// if statement - if empty, run those. otherwise set urlList equal to
+		// input
+
+		// if (args.length == 1) /*
+		// * Do we want to allow for more than 1 document
+		// * reference?
+		// */
+		// // do we want to save the file to the package? or does it already
+		// have to be in the directory?
+		// // add a guard clause in case there isn't a file in the directory
+		// {
+		// try {
+		//
+		// }
+		// catch (InvalidPathException ipe) {
+		// System.err.println("InvalidPathException: " + ipe.getMessage());
+		// }
+
+		// }
+
 		FileReaderObject words = new FileReaderObject(wordsPath);
-
-		Path urlPath = Paths.get(resPath + sep + "urlslist.txt");
-		FileReaderObject urls = new FileReaderObject(urlPath);
-
 		ArrayList<String> wordList = words.sanitizeText(",\"");
-		ArrayList<String> urlList = urls.getRawText();
 
 		URLContentExtractor urlce = new URLContentExtractor();
-		
+
 		for (int i = 0; i < urlList.size(); i++) {
 			ArrayList<String> textArray = new ArrayList<>();
 			ArrayList<String> textWordsArray = new ArrayList<>();
 			HashMap<String, Integer> articleContains = new HashMap<>();
 			ArrayList<String> articleWords = new ArrayList<>();
 
-
 			URL url = new URL(urlList.get(i));
 			String text = urlce.read(url);
 			textArray.add(text);
 
-			textWordsArray = ArrayOrganizer.createArray(textArray,
-					".?! ,()\"");
+			textWordsArray = ArrayOrganizer.createArray(textArray, ".?! ,()\"");
 
 			articleContains = BinarySearcher.search(wordList, textWordsArray);
 
---
+++ ArticleSearch/src/main/java/team/misc/Constants.java
@@ -0,0 +1,6 @@
+package team.misc;
+
+public class Constants {
+	public static final String URLSLIST_TXT = "urlslist.txt";
+	public static final String WORDS_TXT = "words.txt";
+}
